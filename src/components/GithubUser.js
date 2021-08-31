061410827d0bb3f1f87b75f07fbfc709f4e43645
H Brown <hebrown12@gmail.com>
2021-08-31 10:32:11 -0500

    handling promise states

--- src/components/GithubUser.js
+++ src/components/GithubUser.js
@@ -11,33 +11,69 @@ const saveJSON =(key, data)=> {
 
 
 const GithubUser =({ login })=> {
-    const [data, setData] = useState(loadJSON(`user: ${login}`));
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState();
 
-    useEffect(()=> {
-        if (!data) return;
-        if (data.login === login) return;
-        const { name, avatar_url, location } = data;
-        saveJSON(`user: ${login}`, {
-            name,
-            login,
-            avatar_url,
-            location
-        });
-    }, [data]);
+    // useEffect(()=> {
+    //     if (!data) return;
+    //     if (data.login === login) return;
+    //     const { name, avatar_url, location } = data;
+    //     saveJSON(`user: ${login}`, {
+    //         name,
+    //         login,
+    //         avatar_url,
+    //         location
+    //     });
+    // }, [data, login]);
+
+    // useEffect(()=> {
+    //     if (!login) return;
+    //     if (data && data.login === login) return;
+    //     fetch(`https://api.github.com/users/${login}`)
+    //         .then(res => res.json())
+    //         .then(setData)
+    //         .catch(console.error);
+    // }, [data, login]);
 
     useEffect(()=> {
         if (!login) return;
-        if (data && data.login === login) return;
+        setLoading(true);
         fetch(`https://api.github.com/users/${login}`)
-            .then(res => res.json())
+            .then(data => data.json())
             .then(setData)
-            .catch(console.error);
+            .then(()=> setLoading(false))
+            .catch(setError);
     }, [login]);
 
-    if (data)
-        return <pre>{JSON.stringify(data, null, 2)}</pre>
+    if (loading) 
+        return <h1>loading...</h1>
+    if (error)
+        return <pre>{JSON.stringify(error, null, 2)}</pre>
+    if (!data) return null;  
+
+    return (
+        <div className="githubUser">
+            <img
+                src={data.avatar_url}
+                alt={data.login}
+                style={{ width: 200}}
+            />
+            <div>
+                <h1>{data.login}</h1>
+                {data.name && <p>{data.name}</p>}
+                {data.location && <p>{data.location}</p>}
+
+            </div>
+        </div>
+    );
+
     
-        return null;
+
+    // if (data)
+    //     return <pre>{JSON.stringify(data, null, 2)}</pre>
+    
+    //     return null;
 }
 
 
